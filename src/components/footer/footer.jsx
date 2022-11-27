7504a5246e96de8f53859bf5bba85ae676659db0
mylenavendramini <myvendramini@gmail.com>
2022-11-27 18:59:18 +0000

    first commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -36,13 +36,6 @@ function Footer() {
               </li>
             </ul>
           </form>
-
-          <ul className="copyright">
-            <li>&copy; Untitled Inc. All rights reserved.</li>
-            <li>
-              Design: <a href="http://html5up.net">HTML5 UP</a>
-            </li>
-          </ul>
         </div>
       </section>
     </Fragment>
